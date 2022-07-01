d5c389d21b1b104a935d7a1e196c51fb3ea4b1e7
trincaw <58513377+trincaw@users.noreply.github.com>
2022-07-01 17:23:12 +0200

    new version

---
+++ Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GTABooster
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+
+        static void Main()
+        {
+            Task blockInternet = new Task(() => {
+                        Process process = new Process()
+                        {
+                            StartInfo = new ProcessStartInfo()
+                            {
+                                FileName = "ipconfig",
+                                Arguments = "/release",
+                                CreateNoWindow = true,
+                                UseShellExecute = false,
+                                RedirectStandardError = true,
+                                RedirectStandardOutput = true
+                            }
+                        };
+                        process.Start();
+
+                        Thread.Sleep(12000);
+
+                        process.StartInfo.FileName = "ipconfig";
+                        process.StartInfo.Arguments = "/renew";
+                        process.Start();
+                        process.WaitForExit();
+                    });
+
+            Task suspendProcess = new Task(() => {
+                new Program().OptimiseGTA();
+            });
+            Task testReliability = new Task(() => {
+                new Program().OptimiseGTA(500);
+            });
+            while (true)
+            {
+                if (GetAsyncKeyState(0x78) == -32767 && blockInternet.Status != TaskStatus.Running)
+                    blockInternet.Start();
+                if (GetAsyncKeyState(0x79) == -32767 && suspendProcess.Status != TaskStatus.Running)
+                    suspendProcess.Start();
+                if (GetAsyncKeyState(0x7A) == -32767 && suspendProcess.Status != TaskStatus.Running)
+                    testReliability.Start();
+                if (GetAsyncKeyState(0x7B) == -32767)
+                {
+                    while (true)
+                    {
+                        if (blockInternet.Status != TaskStatus.Running && suspendProcess.Status != TaskStatus.Running)
+                            return;
+                    }
+                }
+            }
+        }
+
+        public void OptimiseGTA(int millliseconds = 12000)
+        {
+            int pid = FindProcess();
+            if (pid == -1) return;
+            SuspendProcess(pid);
+            Thread.Sleep(millliseconds);
+            ResumeProcess(pid);
+        }
+        private int FindProcess()
+        {
+            Process currentProcess = Process.GetProcessesByName("GTA5").FirstOrDefault();
+            if (currentProcess == null) return -1;
+            int pid = currentProcess.Id;
+            return pid;
+        }
+        [Flags]
+        private enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+
+        private static void SuspendProcess(int pid)
+        {
+            var process = Process.GetProcessById(pid); // throws exception if process does not exist
+
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                SuspendThread(pOpenThread);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        private static void ResumeProcess(int pid)
+        {
+            var process = Process.GetProcessById(pid);
+
+            if (process.ProcessName == string.Empty)
+                return;
+
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                var suspendCount = 0;
+                do
+                {
+                    suspendCount = ResumeThread(pOpenThread);
+                } while (suspendCount > 0);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,16 +1,8 @@
-# GTABooster
-GTABooster is a free tool for GTA Online that allow you to speed up a lot of long loadings.
-To use this tool you just need to press the button in these cases:
-- Speed up finding online session
-(When you are looking for a session or you are entering gta online wait about 10 seconds and then use this program to skip loading time)
+﻿# GTABooster:
 
-- Protect from cheaters
-(Protect you from cheaters by sending you in solo session fast easy and secure)
+## Usage
 
-- Fix some freeze bugs
-(This program can also sometimes be used to fix small bugs or freezes related to the game session)
-
-Disclamers
-
-This program allows you to solve the problems listed above in the terms described without losing the mission you are doing or other types of contraindications however there is the possibility of ending up in a session on your own or kick other people form your job but alwais without lose your cargo. THIS TOOL IS NOT CONSIDERED A CHEAT AND THERE ARE NO CASES OF BAN TO USE THIS TOOL. Developers are not responsible for the uses of this program.
-
+F9 -> block internet for 12 seconds
+F10 -> suspend gta process for 12 seconds
+F11 -> suspend gta process for 200 milliseconds (used to test the program)
+F12 -> close the program
